beb9a05b313fc8498ea971bd437117c560e77d10
XFSZ <1067068637@qq.com>
2018-01-02 17:02:39 +0800

    2018_1_2

---
+++ cameraray.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraray : MonoBehaviour {
+    public static  GameObject hitgameobject = null;    
+    public float raydistance = 100;      //射线长度
+    Vector3 fwd;                        //物体的前方向
+    // Use this for initialization
+    void Start () {
+        fwd = transform.TransformDirection(Vector3.forward);
+    }
+    void ExplosionDamage(Vector3 center, float radius)                        //球形扫描，目前用不到
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);    //扫描到的物体
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            //被扫到的碰撞体的行为，例如：
+           Debug.Log(hitColliders[i].transform.name);
+            i++;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+      //  Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, raydistance))           //直线扫描
+        {
+            hitgameobject = hit.collider.gameObject;
+            Debug.Log(hitgameobject.transform.parent);
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            Debug.Log(hit.collider.name);
+          //  hitgameobject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.7f);
+        }
+        else {
+            hitgameobject = null;
+        }
+
+            
+     
+
+    }
+}
---
+++ rotate.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    float FromEuler = 0;                        //开始的角度
+    float ToEuler = 60;                        //结束的角度
+    float ResidueAngles = 1;                  //剩余的角度
+
+    bool isMove = false;                    //是否开启 用来位置矫正
+    bool isTimer = false;                  //是否开启  矫正使用的计时器
+    static bool isDisappear = false;      //物体是否应该让图片alpha值下降
+    float timer = 0;                     // 角度矫正计时器
+    int MaxAngles;                      //检测角度是否超出360
+    GameObject hitGO;                  //当前正当中的图片
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log(transform.localEulerAngles.y);
+        if (Input.GetMouseButtonDown(0)) {
+            hitGO = cameraray.hitgameobject;
+        }
+        if (Input.GetMouseButton(0))                        //使用鼠标 进行旋转  方便测试
+        {
+            isMove = false;
+            isTimer = false;
+            transform.Rotate(Vector3.down * Input.GetAxis("Mouse X") * 3, Space.World);
+            hitGO.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.7f);
+            //   FromEuler = transform.localEulerAngles.y;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            timer = 0;                                           //计时器清零
+            isMove = true;                                      // 可以移动
+            isTimer = true;                                    //可以几时
+
+            for (int i = 30; i < 360; i += 60)
+            {
+                MaxAngles = i + 60;
+                Mathf.Clamp(0, 360, MaxAngles);                        //防止超出360
+                if (i < transform.localEulerAngles.y && MaxAngles > transform.localEulerAngles.y)
+                {
+
+                    //    ResidueAngles = i + 60 / transform.localEulerAngles.y;           //根据剩余角度计算时间
+                    FromEuler = transform.localEulerAngles.y;                             // 旋转起始点
+                    ToEuler = i + 30;                                                    //旋转终止点
+
+                }
+                if (transform.localEulerAngles.y < 30 && transform.localEulerAngles.y > 0)
+                {
+                    //       ResidueAngles = i + 60 / transform.localEulerAngles.y;      //根据剩余角度计算时间
+                    FromEuler = transform.localEulerAngles.y;                           // 旋转起始点
+                    ToEuler = 0;                                                       //旋转终止点
+                }
+            }
+            hitGO.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+        }
+
+        //旋转
+        if (Input.touchCount == 1)                                            //触碰测试
+        {
+            //   Ray ray = cameras.ScreenPointToRay(Input.mousePosition);
+            //   RaycastHit hit;
+            // if (Physics.Raycast(ray, out hit))
+            //  {
+            //  if (hit.transform.name == transform.name)
+            //   {
+            if (Input.GetTouch(0).phase == TouchPhase.Began) {
+                hitGO = cameraray.hitgameobject;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)     //手指滑动 物体旋转 但只是按手指的移动速度 而且不会自动矫正 矫正放到LateUpdate中
+            {
+
+                isMove = false;                                      // 可以移动
+                isTimer = false;                                    //可以计时
+                Touch touch = Input.GetTouch(0);
+                Vector2 deltaPos = touch.deltaPosition;
+                transform.Rotate(Vector3.down * deltaPos.x*0.14f , Space.World);      //物体旋转
+
+                
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                timer = 0;                                           //计时器清零
+                isMove = true;                                      // 可以移动
+                isTimer = true;                                    //可以几时
+
+                for (int i = 30; i < 360; i += 60)
+                {
+                    MaxAngles = i + 60;
+                    Mathf.Clamp(0, 360, MaxAngles);           //防止超出360
+                    if (i < transform.localEulerAngles.y && MaxAngles > transform.localEulerAngles.y)
+                    {
+
+                        //    ResidueAngles = i + 60 / transform.localEulerAngles.y;           //根据剩余角度计算时间  暂时不用
+                        FromEuler = transform.localEulerAngles.y;                             // 旋转起始点
+                        ToEuler = i + 30;                                                    //旋转终止点
+
+                    }
+                    if (transform.localEulerAngles.y < 30 && transform.localEulerAngles.y > 0)
+                    {
+                        //       ResidueAngles = i + 60 / transform.localEulerAngles.y;               //根据剩余角度计算时间  暂时不用
+                        FromEuler = transform.localEulerAngles.y;                                    // 旋转起始点
+                        ToEuler = 0;                                                                //旋转终止点
+                    }
+                }
+            }
+            hitGO.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
+            //  }
+
+            //   }
+        }
+
+
+    }
+    private void LateUpdate()
+    {
+        if (isTimer == true)            //是否计时
+        {                               
+            timer += Time.deltaTime;       
+        }
+        if (isMove == true)
+        {
+
+            //物体旋转特定角度
+            this.transform.rotation = Quaternion.Slerp(Quaternion.Euler(new Vector3(0, FromEuler, 0)), Quaternion.Euler(new Vector3(0, ToEuler, 0)), 1f * timer * ResidueAngles);
+        }
+        if (timer > 5)
+        {              //计时超过5秒自动停止
+
+            isMove = false;
+            isTimer = false;
+
+        }
+
+
+    }
+}
